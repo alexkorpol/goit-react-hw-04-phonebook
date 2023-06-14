d6b3aa30dc4e3c0eed27b4efd14f5d30adaf8c4f
Alexander <alexkorpol1964@gmail.com>
2023-06-14 14:21:07 +0300

    Fixed note of mentor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,17 +17,42 @@ function App(){
     const addNewContact = ({ nameNew, numberNew}) => {
       const newNameToLowerCase = nameNew.toLowerCase();
 
-     let index = -1
-     index = (contacts.findIndex(({ name, number }) => (name.toLowerCase() === newNameToLowerCase && number === numberNew)))
-    if(index !== -1){
-        Notify.failure(`${contacts[index].name} and number ${contacts[index].number}   is already in list contacts`);
-        console.log("index >>>", index);
+    //  let index = -1
+    //  index = (contacts.findIndex(({ name, number }) => (name.toLowerCase() === newNameToLowerCase && number === numberNew)))
+    // if(index !== -1){
+    //     Notify.failure(`${contacts[index].name} and number ${contacts[index].number}   is already in list contacts`);
+    //     console.log("index >>>", index);
+    //     return;
+    //     }
+
+    // index = (contacts.findIndex(({ number }) => (number === numberNew)))
+    // if(index !== -1) {
+    //     Notify.failure(`This number ${contacts[index].number} is already in list contacts ${contacts[index].name}`);
+    //     return;
+    //   }
+
+// ! ====================== НОТАТКА ДЛЯ МЕНТОРА! =========================================
+// ! Зверніть будь-ласка увагу, що я не використовую  метод для пошуку по масиву find з той причини, що
+// ! результатом його роботи є перший знайдений елемент за умовою у колл-бек функції.
+// ! Для відображення повідомлення користувачу з використанням  Notify, у якому відображаються
+// ! ІМ'Я та НОМЕРУ З реально ІСНУЮЧОГО МАСИВА, я використовую метод findIndex,
+// ! якій якраз і повертає номер знайденого індексу з масиву contacts,
+// ! який як раз мені і потрібен для задуманого мною відпрацювання відображення повідомлення користувачу.
+// ! Тому і в умові (nameAndNumberIsExistIndex >= 0) опретора if задається >=0 (а не просто if(nameAndNumberIsExistIndex)),
+// ! бо якщо дійсно новий контакт заводиться, то вказана у nameAndNumberIsExistIndex умова в цьому разі поверне значення -1
+// ! (індекс не знайдено), і тоді вираз у операторі if спрацює НЕКОРЕКТНО, бо не знайде елемент масиву з номером індексу -1.
+// !
+// ! Те саме стосується і перевірки введеного номеру телефону на існування у існуючому масиві.
+
+      const nameAndNumberIsExistIndex = (contacts.findIndex(({ name, number }) => (name.toLowerCase() === newNameToLowerCase && number === numberNew)))
+        if (nameAndNumberIsExistIndex >= 0) {
+        Notify.failure(`${contacts[nameAndNumberIsExistIndex].name} and number ${contacts[nameAndNumberIsExistIndex].number}   is already in list contacts`);
         return;
         }
 
-    index = (contacts.findIndex(({ number }) => (number === numberNew)))
-    if(index !== -1) {
-        Notify.failure(`This number ${contacts[index].number} is already in list contacts ${contacts[index].name}`);
+      const numberIsExistIndex = (contacts.findIndex(({ number }) => (number === numberNew)))
+    if(numberIsExistIndex >= 0) {
+        Notify.failure(`This number ${contacts[numberIsExistIndex].number} is already in list contacts ${contacts[numberIsExistIndex].name}`);
         return;
       }
 
